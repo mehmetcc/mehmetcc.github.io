8dbbb99e03b39efdc762ec60b2678a0a09daecc3
mehmetcc <mehmetccm@gmail.com>
2025-12-24 16:20:37 +0300

    modify config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import rehypePrettyCode from 'rehype-pretty-code';
-import github from '@astrojs/github-pages';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://mehmetcan.io',
-  integrations: [github()],
   markdown: {
     rehypePlugins: [
       [rehypePrettyCode, {
